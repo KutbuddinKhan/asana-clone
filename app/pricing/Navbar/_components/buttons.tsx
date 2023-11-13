0f0f94e59e1b077d7ca65272f86c53ae7ff78950
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-11-14 01:11:22 +0530

    adding price page

--- app/pricing/Navbar/_components/buttons.tsx
+++ app/pricing/Navbar/_components/buttons.tsx
@@ -40,7 +40,7 @@ const ActionButtons = () => {
                                         Get Started
                                     </Link>
                                     <Link
-                                        href={'/'}
+                                        href={'/pricing'}
                                         passHref
                                     >
                                         Pricing
